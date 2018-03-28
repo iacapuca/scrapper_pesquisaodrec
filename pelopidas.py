c90e563f7dd265e4de4dfcf7a604fa2fe8d76f77
root <root@DESKTOP-8I67NLH.localdomain>
2018-03-28 02:39:56 -0300

    Version 1.0

--- pelopidas.py
+++ pelopidas.py
@@ -13,10 +13,26 @@ client = MongoClient(config.mongoURL)
 db = client.ameciclo
 pesquisa = db.pesquisa_od
 
+
 for doc in pesquisa.find({"protocolo": ''}):
+    protocolo = None
+    errors = None
     r = requests.post(config.URL, data=doc)
     print (r.history)
+    id = doc["_id"]
+    print (id)
     soup = BeautifulSoup(r.text, 'html.parser')
-    protocolo = soup.em.string
-    print(protocolo)
-    time.sleep(5)
+    try:
+        errors = soup.find('ul', attrs={'class':'messages__list'}).text.strip()
+    except(RuntimeError, TypeError, NameError, AttributeError):
+        print("Nenhum erro encontrado.")
+    if errors is not None:
+        print (errors)
+    try:
+        protocolo = soup.em.string
+    except(RuntimeError, TypeError, NameError, AttributeError):
+        print("Não foi possivel encontrar protocolo, possivel erro de preenchimento.")
+    if protocolo is not None:
+        print(protocolo)
+        pesquisa.update_one({'_id':id},{'$set':{'protocolo': protocolo}})
+    time.sleep(180)
